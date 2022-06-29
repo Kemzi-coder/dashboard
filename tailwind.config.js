887585e3f94f9905de0cc4320a02e8ee055b1124
maxkemzi <kemzi9585@gmail.com>
2022-06-29 16:51:11 +0300

    Add Tailwind CSS library

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#13161E",
+      primaryLighter: "#262931",
+      primaryLight: "#6C6C77",
+      accent: "#1F99C9",
+      white: "#FFFFFF",
+      transparent: "transparent"
+    },
+  },
+  plugins: [],
+}
