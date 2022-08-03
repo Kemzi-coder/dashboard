38d3b0d96c0375c8dce7c260301e3dfd75de3ca8
Max Kemzi <kemzi9585@gmail.com>
2022-08-03 11:44:08 +0300

    Add endOfLine rule to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
 	plugins: ["react", "prettier"],
 	rules: {
-		"prettier/prettier": "error",
+		"prettier/prettier": ["error", {endOfLine: "auto"}],
 		"react/function-component-definition": [
 			"error",
 			{
