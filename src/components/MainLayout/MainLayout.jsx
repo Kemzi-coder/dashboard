c571b423b2068943f2a4f0e85cb4daab22d7de94
Max Kemzi <kemzi9585@gmail.com>
2022-08-04 17:16:55 +0300

    Fix page scoll

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -4,7 +4,7 @@ import LogoSection from "../../views/LogoSection/LogoSection";
 import SidebarSection from "../../views/SidebarSection/SidebarSection";
 
 const MainLayout = ({children}) => (
-	<div className="grid gap-px grid-cols-[300px_1fr] grid-rows-[auto_minmax(900px,_1fr)] bg-primaryLighter text-white">
+	<div className="grid min-h-screen gap-px grid-cols-[300px_1fr] grid-rows-[auto_1fr] bg-primaryLighter text-white">
 		<LogoSection />
 		{children}
 		<SidebarSection />
