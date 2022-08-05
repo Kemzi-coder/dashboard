aa4a9ec1412e1d5be1fa83d3c0a766c13e292204
Max Kemzi <kemzi9585@gmail.com>
2022-08-05 11:40:11 +0300

    Improve avatar component look

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
-const Avatar = ({imagePath}) => <img src={imagePath} alt="avatar" />;
+const Avatar = ({imagePath, width, height, className}) => (
+	<img
+		className={classNames(className, "rounded-full object-cover block")}
+		width={width}
+		height={height}
+		src={imagePath}
+		alt="avatar"
+	/>
+);
 
 Avatar.propTypes = {
+	width: PropTypes.number,
+	height: PropTypes.number,
+	className: PropTypes.string,
 	imagePath: PropTypes.string.isRequired
+};
+
+Avatar.defaultProps = {
+	width: 40,
+	height: 40,
+	className: ""
 };
 
 export default Avatar;
