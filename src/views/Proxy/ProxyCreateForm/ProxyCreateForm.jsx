17d76d89c6f5af10ed93b5f41f3156956482c490
Max Kemzi <kemzi9585@gmail.com>
2022-08-06 17:58:21 +0300

    Add proxy create form component

---
+++ src/views/Proxy/ProxyCreateForm/ProxyCreateForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {useForm} from "react-hook-form";
+import Button from "../../../components/Button/Button";
+import FormFields from "../../../components/FormFields/FormFields";
+import Proxies from "../../../store/proxies";
+import FormTextField from "../../../components/FormTextField/FormTextField";
+import createFormValidation from "../../../validation/proxies";
+
+const ProxyCreateForm = () => {
+	const {
+		handleSubmit,
+		register,
+		formState: {errors, isSubmitting}
+	} = useForm({mode: "all"});
+
+	const onSubmit = async data => {
+		await Proxies.create(data);
+	};
+
+	return (
+		<>
+			<h2 className="text-white text-4xl mb-4 font-semibold">Create proxy</h2>
+			<form className="max-w-sm" onSubmit={handleSubmit(onSubmit)}>
+				<FormFields className="mb-6" register={register} errors={errors}>
+					<FormTextField
+						placeholder="Type"
+						name="proxy_type"
+						options={createFormValidation.proxy_type}
+					/>
+					<FormTextField
+						placeholder="Host"
+						name="proxy_host"
+						options={createFormValidation.proxy_host}
+					/>
+					<FormTextField
+						placeholder="Port"
+						name="proxy_port"
+						options={createFormValidation.proxy_port}
+					/>
+					<FormTextField placeholder="Username" name="proxy_username" />
+					<FormTextField
+						placeholder="Password"
+						name="proxy_password"
+						type="password"
+					/>
+				</FormFields>
+				<Button disabled={isSubmitting} type="submit">
+					Create
+				</Button>
+			</form>
+		</>
+	);
+};
+
+export default ProxyCreateForm;
