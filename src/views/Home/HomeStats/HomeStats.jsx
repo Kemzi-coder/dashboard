934f802ab388db7faab526bf265db1c870840fab
Max Kemzi <kemzi9585@gmail.com>
2022-08-08 18:19:56 +0300

    Sort statistics in descending order

--- src/views/Home/HomeStats/HomeStats.jsx
+++ src/views/Home/HomeStats/HomeStats.jsx
@@ -1,10 +1,14 @@
 import {observer} from "mobx-react-lite";
-import React from "react";
+import React, {useMemo} from "react";
 import StatItem from "../../../components/StatItem/StatItem";
 import Stats from "../../../store/stats";
 
 const HomeStats = observer(() => {
 	const {stats, isLoading} = Stats;
+	const sortedStats = useMemo(
+		() => Object.keys(stats).sort((a, b) => stats[a] < stats[b]),
+		[stats]
+	);
 
 	return (
 		<>
@@ -13,7 +17,7 @@ const HomeStats = observer(() => {
 				<div>Loading...</div>
 			) : (
 				<div className="gap-4 grid xl:grid-cols-4 lg:grid-cols-3 lg:gap-8 md:grid-cols-2">
-					{Object.keys(stats).map(key => (
+					{sortedStats.map(key => (
 						<StatItem key={key} value={stats[key]} title={`${key} Accounts`} />
 					))}
 				</div>
