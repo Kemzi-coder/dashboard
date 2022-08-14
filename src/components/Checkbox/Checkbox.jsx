125c0a9ffd1ad1c9726b835e6790ac14487fa8b9
Max Kemzi <kemzi9585@gmail.com>
2022-08-14 17:24:08 +0300

    Change checkbox orientation to row

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,7 +3,13 @@ import PropTypes from "prop-types";
 import React, {forwardRef} from "react";
 
 const Checkbox = forwardRef(({text, name, className, ...props}, ref) => (
-	<label className={classNames(className, "cursor-pointer")} htmlFor={name}>
+	<label
+		className={classNames(
+			className,
+			"cursor-pointer flex justify-between items-center"
+		)}
+		htmlFor={name}
+	>
 		<input
 			ref={ref}
 			id={name}
@@ -12,8 +18,8 @@ const Checkbox = forwardRef(({text, name, className, ...props}, ref) => (
 			type="checkbox"
 			{...props}
 		/>
-		<span className="mb-2 inline-block text-lg">{text}</span>
-		<span className="relative block peer-checked:before:translate-x-full peer-checked:before:bg-accent overflow-hidden w-14 h-7 bg-primary-light rounded-full mr-2 before:absolute before:block before:w-1/2 before:h-full before:bg-text-dark before:rounded-full before:left-0 before:top-0 before:transition-colors before:transition-transform before:border-[3px] before:border-primary-light" />
+		<span className="mr-4 inline-block text-lg">{text}</span>
+		<span className="relative inline-block peer-checked:before:translate-x-full peer-checked:before:bg-accent overflow-hidden w-14 h-7 bg-primary-light rounded-full before:absolute before:block before:w-1/2 before:h-full before:bg-text-dark before:rounded-full before:left-0 before:top-0 before:transition-colors before:transition-transform before:border-[3px] before:border-primary-light" />
 	</label>
 ));
 
