ea40d87cd4f9331e768338da9dc1d874ed8077e5
Max Kemzi <kemzi9585@gmail.com>
2022-08-15 15:58:50 +0300

    Add className prop for input Needed to pass styles

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,39 +2,43 @@ import React, {forwardRef} from "react";
 import PropTypes, {bool} from "prop-types";
 import classNames from "classnames";
 
-const Input = forwardRef(({startIcon: Icon, isInvalid, ...props}, ref) => (
-	<div className="relative flex items-center">
-		<input
-			ref={ref}
-			className={classNames(
-				"peer text-lg w-full transition-colors rounded-base bg-primary-light border py-2 text-text-light placeholder:text-text-darker",
-				{
-					"border-danger": isInvalid,
-					"border-transparent": !isInvalid,
-					"pl-12 pr-4": !!Icon,
-					"px-4 peer": !Icon
-				}
-			)}
-			type="text"
-			{...props}
-		/>
-		{Icon && (
-			<Icon
-				className="peer-focus:text-text-light transition-colors absolute left-4 text-text-dark"
-				size={22}
+const Input = forwardRef(
+	({className, startIcon: Icon, isInvalid, ...props}, ref) => (
+		<div className={classNames(className, "relative flex items-center")}>
+			<input
+				ref={ref}
+				className={classNames(
+					"peer text-lg w-full transition-colors rounded-base bg-primary-light border py-2 text-text-light placeholder:text-text-darker",
+					{
+						"border-danger": isInvalid,
+						"border-transparent": !isInvalid,
+						"pl-12 pr-4": !!Icon,
+						"px-4 peer": !Icon
+					}
+				)}
+				type="text"
+				{...props}
 			/>
-		)}
-	</div>
-));
+			{Icon && (
+				<Icon
+					className="peer-focus:text-text-dark transition-colors absolute left-4 text-text-darker"
+					size={22}
+				/>
+			)}
+		</div>
+	)
+);
 
 Input.propTypes = {
 	startIcon: PropTypes.elementType,
-	isInvalid: bool
+	isInvalid: bool,
+	className: PropTypes.string
 };
 
 Input.defaultProps = {
 	startIcon: null,
-	isInvalid: false
+	isInvalid: false,
+	className: ""
 };
 
 export default Input;
