8a7da92aff8e955c5dc9e387cc380d5d781bf5ae
Max Kemzi <kemzi9585@gmail.com>
2022-08-16 20:20:47 +0300

    Add active chat feature

--- src/views/Chat/ChatSidebar/ChatSidebar.jsx
+++ src/views/Chat/ChatSidebar/ChatSidebar.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect} from "react";
 import {MdSearch} from "react-icons/md";
 import {observer} from "mobx-react-lite";
+import {useParams} from "react-router-dom";
 import BurgerButton from "../../../components/BurgerButton/BurgerButton";
 import Input from "../../../components/Input/Input";
 import ChatItem from "../../../components/ChatItem/ChatItem";
 import chats from "../../../store/chats";
 
 const ChatSidebar = observer(() => {
+	const {id} = useParams();
+
 	useEffect(() => {
 		chats.fetch();
 	}, []);
@@ -17,15 +20,20 @@ const ChatSidebar = observer(() => {
 				<BurgerButton className="mr-4 shrink-0" />
 				<Input startIcon={MdSearch} placeholder="Search" />
 			</div>
+			{!chats.isLoading && chats.chats.length === 0 && (
+				<div className="mt-2 text-center">There are no chats.</div>
+			)}
 			{chats.isLoading ? (
 				<div className="mt-2 text-center">Loading...</div>
 			) : (
 				<div>
 					{chats.chats.map(chat => (
 						<ChatItem
+							isActive={id === chat.chat_id}
+							chatId={chat.chat_id}
 							title={chat.chat_title}
 							lastMsgDate={chat.parsed_at}
-							key={chat.id}
+							key={chat.chat_id}
 						/>
 					))}
 				</div>
