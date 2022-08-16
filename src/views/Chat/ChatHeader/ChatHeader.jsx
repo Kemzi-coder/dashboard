e32f0ad33537b3937e710bf3b730cbfd31fa6b12
Max Kemzi <kemzi9585@gmail.com>
2022-08-16 20:24:16 +0300

    Add navigation to previous path functionality

--- src/views/Chat/ChatHeader/ChatHeader.jsx
+++ src/views/Chat/ChatHeader/ChatHeader.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import {MdArrowBack} from "react-icons/md";
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 import Avatar from "../../../components/Avatar/Avatar";
-import {HOME_ROUTE} from "../../../utils/constants/routes";
+import {ACCOUNTS_ROUTE} from "../../../utils/constants/routes";
 
-const ChatHeader = () => (
-	<div className="flex items-center justify-between border-b border-r border-primary-lighter p-6">
-		<div className="flex items-center">
-			<Avatar
-				className="mr-4"
-				width={50}
-				height={50}
-				imagePath=""
-				username="maxkemzi"
-			/>
-			<p>maxkemzi</p>
+const ChatHeader = () => {
+	const location = useLocation();
+	return (
+		<div className="flex items-center justify-between border-b border-r border-primary-lighter p-6">
+			<div className="flex items-center">
+				<Avatar
+					className="mr-4"
+					width={50}
+					height={50}
+					imagePath=""
+					username="maxkemzi"
+				/>
+				<p>maxkemzi</p>
+			</div>
+			<NavLink to={location?.state?.prevPath || ACCOUNTS_ROUTE}>
+				<MdArrowBack className="text-text-dark" size={22} />
+			</NavLink>
 		</div>
-		<NavLink to={HOME_ROUTE}>
-			<MdArrowBack className="text-text-dark" size={22} />
-		</NavLink>
-	</div>
-);
+	);
+};
 
 export default ChatHeader;
